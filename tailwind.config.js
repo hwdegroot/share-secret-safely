44d178c54df96e3cbfc3da2bac3f34b97d6c6ba7
Rik de Groot <hwdegroot@gmail.com>
2023-08-01 12:21:51 +0200

    Upgrade npm dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
     content: [
         './wsgi_app/templates/**/*.{html,js,svg}',
     ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
+
